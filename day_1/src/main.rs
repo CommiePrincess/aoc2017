fe03a9bf5648be167d5c3e4d19568b642bea0768
Commie Princess <34149625+CommiePrincess@users.noreply.github.com>
2017-12-02 11:15:49 -0500

    general refactoring

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -3,8 +3,8 @@ fn main() {
 
     let input : Vec<u32> = input.chars().map(|c| c.to_digit(10).unwrap()).collect();
 
-    let part_1 = |input_vec: &Vec<u32>| (0..(input_vec.len())).filter(|i| input_vec[*i] == input_vec[(i + 1) % input_vec.len()]).map(|e| input_vec[e]).sum::<u32>();
-    let part_2 = |input_vec: &Vec<u32>| (0..(input_vec.len() / 2)).filter(|i| input_vec[*i] == input_vec[i + (input_vec.len() / 2)]).map(|e| input_vec[e] * 2).sum::<u32>();
+    let part_1 = |input_vec: &[u32]| (0..(input_vec.len())).filter(|i| input_vec[*i] == input_vec[(i + 1) % input_vec.len()]).map(|e| input_vec[e]).sum::<u32>();
+    let part_2 = |input_vec: &[u32]| (0..(input_vec.len() / 2)).filter(|i| input_vec[*i] == input_vec[i + (input_vec.len() / 2)]).map(|e| input_vec[e] * 2).sum::<u32>();
 
     println!("solution to part 1: {}, solution to part 2: {}", part_1(&input), part_2(&input));
 }
--- warmup/src/main.rs
+++ warmup/src/main.rs
@@ -1,4 +1,9 @@
-#[derive(Debug)]
+enum Marker {
+    A,
+    B,
+    Null,
+}
+
 struct Position {
     x : i32,
     y : i32,
@@ -11,16 +16,9 @@ impl Position {
         self.y += dir.1;
     }
 
-    fn distance (&self, point: &Position) -> i32 {
-        ((self.x - point.x).abs() + (self.y - point.y).abs())
+    fn distance (&self, point: &Position) -> u32 {
+        ((self.x - point.x).abs() + (self.y - point.y).abs()) as u32
     }
-}
-
-#[derive(Debug)]
-enum Marker {
-    A,
-    B,
-    Null,
 }
 
 fn main() {
@@ -44,29 +42,19 @@ fn main() {
         } 
     }
 
-    {
-        let mut farthest_from_origin : &Position = &origin;
+    let part_1_answer : u32 = markers.iter().map(|m| m.distance(&origin)).max().unwrap() as u32;
 
-        for marker in &markers {
-            if marker.distance(&origin) > farthest_from_origin.distance(&origin) {
-                farthest_from_origin = &marker;
-            }
+    let mut longest_distance : u32 = 0;
+
+    let (a_markers, b_markers) : (Vec<&Position>, Vec<&Position>) = markers.iter().partition(|x| match x.marker_type {Marker::A => true, _ => false});
+
+    a_markers.iter().for_each(|a| b_markers.iter().for_each(|b| {
+        if a.distance(b) > longest_distance {
+            longest_distance = a.distance(b);
         }
+    }));
 
-        println!("{:?}, with distance {}", farthest_from_origin, farthest_from_origin.distance(&origin));
-    }
+    let part_2_answer : u32 = longest_distance;
 
-    {
-        let mut farthest_pair : (&Position, &Position) = (&origin, &origin);
-
-        let (a_markers, b_markers) : (Vec<&Position>, Vec<&Position>) = markers.iter().partition(|x| match x.marker_type {Marker::A => true, _ => false});
-
-        a_markers.iter().for_each(|a| b_markers.iter().for_each(|b| {
-            if &a.distance(&b) > &farthest_pair.0.distance(&farthest_pair.1) {
-                farthest_pair = (&a, &b);
-            }
-        }));
-
-        println!("{:?}, with distance {}", farthest_pair, farthest_pair.0.distance(farthest_pair.1));
-    }
+    println!("answer for part 1: {}\nanswer for part 2: {}", part_1_answer, part_2_answer);
 }
